5d7263638c3d91f6c1be62deedb72c71d0be6768
Gabriel Torres Martins <gabrieltm06@hotmail.com>
2026-09-07 01:07:53 -0300

    Indo para JS

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,16 @@
 const formCampo = document.querySelector("#campTudoInput");
 const campoCriar = document.querySelector("#criarListaCamp");
 const inputCriar = document.querySelector("#icriar");
-const campFiltro = document.querySelector("#campPesquisar");
-const inputFiltro = document.querySelector("#ipesquisa");
-const formCampo = document.querySelector("#campTudoInput");
+const campEdicao = document.querySelector("#editarListaCamp");
 const botaoCancelar = document.querySelector("#cancelarButton");
+
+
+formCampo.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const criarInput = icriar.value;
+
+    if (criarInput) {
+        console.log(criarInput);
+    }
+})
